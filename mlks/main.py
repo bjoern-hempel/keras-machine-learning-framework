01e3d58015ba73be5cb4926e5f74f7c4f0eb14c5
Björn Hempel <bjoern@hempel.li>
2019-09-22 19:26:17 +0200

    refactoring

--- mlks/main.py
+++ mlks/main.py
@@ -43,28 +43,6 @@ from mlks.helper.config import general_config_writer, machine_learning_config_wr
 from mlks.helper.config import option_callback, add_options
 from mlks.helper.config import set_config_translator
 
-# Make pass decorator for class Config
-pass_config = click.make_pass_decorator(Config, ensure=True)
-
-# sets the config translator
-set_config_translator({
-    # general config
-    'verbose': general_config_writer,
-    'debug': general_config_writer,
-
-    # machine learning config
-    'epochs': machine_learning_config_writer,
-    'learning_rate': machine_learning_config_writer,
-    'activation_function': machine_learning_config_writer,
-    'loss_function': machine_learning_config_writer,
-    'optimizer': machine_learning_config_writer,
-    'metrics': machine_learning_config_writer,
-
-    # transfer learning config
-    'transfer_learning_model': transfer_learning_config_writer,
-    'number_trainable_layers': transfer_learning_config_writer
-})
-
 
 # Configure the universal parameters here
 option_verbose = click.option('--verbose', '-v',
@@ -77,6 +55,7 @@ option_debug = click.option('--debug', '-d',
                             is_flag=True,
                             help='Switches the script to debug mode.',
                             callback=option_callback)
+
 
 # Configure the machine learning parameters here
 option_epochs = click.option('--epochs', '-e',
@@ -122,6 +101,7 @@ option_metrics = click.option('--metrics',
                               default='accuracy',
                               type=str)
 
+
 # Configure the transfer learning parameters here
 option_transfer_learning_model = click.option('--transfer-learning-model', '-m',
                                               expose_value=False,
@@ -138,11 +118,36 @@ option_number_trainable_layers = click.option('--number-trainable-layers',
                                               default=3,
                                               type=int)
 
+
 # Configure some option sets
 option_set_general = [option_verbose, option_debug]
 option_set_machine_learning = [option_epochs, option_learning_rate, option_activation_function, option_loss_function,
                                option_optimizer, option_metrics]
 option_set_transfer_learning = [option_transfer_learning_model, option_number_trainable_layers]
+
+
+# sets the config translator
+set_config_translator({
+    # general config
+    'verbose': general_config_writer,
+    'debug': general_config_writer,
+
+    # machine learning config
+    'epochs': machine_learning_config_writer,
+    'learning_rate': machine_learning_config_writer,
+    'activation_function': machine_learning_config_writer,
+    'loss_function': machine_learning_config_writer,
+    'optimizer': machine_learning_config_writer,
+    'metrics': machine_learning_config_writer,
+
+    # transfer learning config
+    'transfer_learning_model': transfer_learning_config_writer,
+    'number_trainable_layers': transfer_learning_config_writer
+})
+
+
+# Make pass decorator for class Config
+pass_config = click.make_pass_decorator(Config, ensure=True)
 
 
 @click.group()
