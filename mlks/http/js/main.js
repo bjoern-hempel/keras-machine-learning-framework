87907f6fd58e92696e1ce02ff05dffff7c45aaea
Björn Hempel <bjoern@hempel.li>
2019-11-05 09:19:56 +0100

    some js improvements

--- mlks/http/js/main.js
+++ mlks/http/js/main.js
@@ -17,65 +17,79 @@ window.submitPicture = function () {
 
     /* continue uploading (true) */
     return true;
-}
+};
 
-window.renewPredictionOverview = function(database, evaluationData, titleTemplate, classTemplate, classTemplateExtra) {
+window.getName = function(database, className, language) {
+    let classes = database['classes'];
+
+    if (className in classes && classes[className]['name'][language]) {
+        return '<b>' + classes[className]['name'][language] + '</b>';
+    }
+
+    return '<b>' + className + '</b>';
+};
+
+window.getExtraInformationText = function(database, className, language) {
+    let classes = database['classes'];
+    let categories = database['categories'];
+    let extraInformationText = '';
+
+    if (!(className in classes)) {
+        return extraInformationText;
+    }
+
+    if (classes[className]['description'][language]) {
+        extraInformationText = classes[className]['description'][language];
+    }
+
+    if (classes[className]['urls']['wikipedia'][language]) {
+        extraInformationText += extraInformationText ? '<br />' : '';
+
+        extraInformationText += '<b>Wikipedia:</b> </b>' +
+            '<a href="' + classes[className]['urls']['wikipedia'][language] + '" target="_blank">' +
+            classes[className]['urls']['wikipedia'][language] +
+            '</a>'
+        ;
+    }
+
+    let categoriesCurrent = classes[className]['categories'];
+
+    if (categoriesCurrent.length > 0) {
+        extraInformationText += '<br />';
+    }
+
+    for (let categoryId in categoriesCurrent) {
+        let categoryCurrent = categoriesCurrent[categoryId];
+
+        extraInformationText += extraInformationText ? '<br />' : '';
+        extraInformationText += '<b>Category:</b> ' +
+            '<a href="' + categories[categoryCurrent]['urls']['wikipedia'][language] + '" target="_blank">' +
+                categories[categoryCurrent]['name'][language] +
+            '</a>'
+        ;
+    }
+
+    return extraInformationText;
+};
+
+window.renewPredictionOverview = function(database, evaluationData, titleTemplate, classTemplate, classTemplateExtraInformation) {
     let html = titleTemplate;
     let language = 'GB';
 
     for (let index in evaluationData['prediction_overview_array']) {
         let predictionItem = evaluationData['prediction_overview_array'][index];
-        let className = predictionItem['class_name'].replace(':', '');
+        let className = predictionItem['class_name'].replace(/:$/, '');
         let percent = Math.round(predictionItem['predicted_value'] * 100 * 100) / 100;
-        let classes = database['classes'];
-        let categories = database['categories'];
-        let name = '<b>' + className + '</b>';
+        let name = window.getName(database, className, language);
+        let extraInformationText = window.getExtraInformationText(database, className, language);
+        let predictionHtml = '';
 
-        if (className in classes && classes[className]['name'][language]) {
-            name = '<b>' + classes[className]['name'][language] + '</b>';
-        }
-
-        var predictionHtml = classTemplate.
+        /* add html templates */
+        predictionHtml += classTemplate.
             replace(/%\(name\)s/, name).
             replace(/%\(percent\)s/, percent + '%');
-
-        if (className in classes) {
-            let description = '';
-
-            if (classes[className]['description'][language]) {
-                description = classes[className]['description'][language];
-            }
-
-            if (classes[className]['urls']['wikipedia'][language]) {
-                description += description ? '<br />' : '';
-
-                description += '<b>Wikipedia:</b> </b>' +
-                    '<a href="' + classes[className]['urls']['wikipedia'][language] + '" target="_blank">' +
-                    classes[className]['urls']['wikipedia'][language] +
-                    '</a>'
-                ;
-            }
-
-            let categoriesCurrent = classes[className]['categories'];
-
-            if (categoriesCurrent.length > 0) {
-                description += description ? '<br />' : '';
-            }
-
-            for (let categoryId in categoriesCurrent) {
-                let categoryCurrent = categoriesCurrent[categoryId];
-
-                description += description ? '<br />' : '';
-                description += '<b>Category:</b> ' +
-                    '<a href="' + categories[categoryCurrent]['urls']['wikipedia'][language] + '" target="_blank">' +
-                        categories[categoryCurrent]['name'][language] +
-                    '</a>'
-                ;
-            }
-
-            predictionHtml += classTemplateExtra.
-                replace(/%\(more\)s/, description);
-        }
+        predictionHtml += classTemplateExtraInformation.
+            replace(/%\(more\)s/, extraInformationText);
 
         html += predictionHtml
     }
