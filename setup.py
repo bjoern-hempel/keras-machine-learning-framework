60753f774a3582faf947c47694b69c86a393d1b6
bjoern-hempel <bjoern@hempel.li>
2020-03-15 20:37:41 +0100

    setup.py adjustments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     py_modules=['mlks.main'],
     install_requires=[
-        'Click', 'keras', 'matplotlib', 'numpy', 'tensorflow', 'scikit-learn', 'pandas', 'seaborn', 'six'
+        'Click', 'matplotlib', 'numpy', 'tensorflow', 'scikit-learn', 'pandas', 'seaborn', 'six'
     ],
     entry_points='''
         [console_scripts]
